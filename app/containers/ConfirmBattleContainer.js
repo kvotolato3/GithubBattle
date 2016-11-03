d197c44f686b6e7c56244188eee7165eb52096fb
Kathleen Votolato <kvotolato3@gmail.com>
2016-11-03 15:12:11 -0400

    Understanding React Lifecycle events
    - Added Confirm Battle Container and Confirm Battle presentation component
    - Added a new route
    - Added console.logs for several commonly used life cycle events, to
    see when they are triggered.

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,39 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    console.log('getInitialState');
+    return {
+      isLoading: false,
+      playerInfo: []
+    }
+  },
+  componentWillMount: function () {
+    console.log('componentWillMount');
+  },
+  componentDidMount: function () {
+    var query = this.props.location.query;
+    console.log('componentDidMount');
+    //Fetch info from github, then update state
+  },
+  componentWillReceiveProps: function () {
+    console.log('componentWillReceiveProps');
+  },
+  componentWillUnmount: function () {
+    console.log('componentWillUnmount');
+  },
+  render: function () {
+    return (
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        playerInfo={this.state.playerInfo} />
+    );
+  }
+});
+
+module.exports = ConfirmBattleContainer;
+
