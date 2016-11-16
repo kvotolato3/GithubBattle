bb0daaf6bd0fe9bd85357228c9ce9b08b9dd2359
Kathleen Votolato <kvotolato3@gmail.com>
2016-11-16 16:00:20 -0500

    Added style-loader and css-loader to the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      {test: /\.css$/, loader: "style-loader!css-loader"}
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
